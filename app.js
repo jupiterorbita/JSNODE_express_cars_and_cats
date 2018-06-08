92143683863bcd168c57fd183cd8d9cb94397616
jupterorbita <john.williams@outlook.com>
2018-06-07 23:39:34 -0700

    express_cars_and_cats NODEJS

---
+++ app.js
@@ -0,0 +1,8 @@
+//express_cars_and_cats
+var express = require("express");
+var app = express();
+app.use(express.static(__dirname + "/static"));
+
+app.listen(8000, function(){
+    console.log("listening on 8000")
+})
